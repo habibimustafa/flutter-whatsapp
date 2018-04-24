9252adbbf5a94d5e1ae896551d7d225160ec27cb
Habibi Mustafa <bibitahabibi@gmail.com>
2018-04-24 21:30:24 +0700

    add mute chat icon

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -13,7 +13,7 @@ class ChatsPageState extends State<ChatsPage> {
       itemCount: dummyData.length,
       itemBuilder: (context, i) => new Column(
         children: <Widget>[
-          new Divider(height: 10.0),
+          new Divider(height: 5.0),
           new ListTile(
             leading: new CircleAvatar(
               foregroundColor: Theme.of(context).primaryColor,
@@ -35,10 +35,16 @@ class ChatsPageState extends State<ChatsPage> {
             ),
             subtitle: new Container(
               padding: const EdgeInsets.only(top: 5.0),
-              child: new Text(
-                dummyData[i].message,
-                style: new TextStyle(color: Colors.grey, fontSize: 15.0),
-              ),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  new Text(
+                    dummyData[i].message,
+                    style: new TextStyle(color: Colors.grey, fontSize: 15.0),
+                  ),
+                  new Icon(Icons.volume_off)
+                ],
+              )
             ),
           )
         ],
